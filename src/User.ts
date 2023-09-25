bd629e709f87e214685b81a4d9e7afd10e98582a
Dr.Alwin Simon <003alwin@gmail.com>
2023-09-25 15:56:58 +0530

    Configured User with Faker Data

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,17 @@
-class User {}
+import { faker } from "@faker-js/faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.person.firstName();
+    this.location = {
+      lat: parseFloat(faker.location.latitude()),
+      lng: parseFloat(faker.location.longitude()),
+    };
+  }
+}
