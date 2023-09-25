1a1f4b3ef6054a61b119ccd23bdce5dd2a3c5dfb
Dr.Alwin Simon <003alwin@gmail.com>
2023-09-25 19:29:54 +0530

    Configured Class for Custom Map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user1 = new User();
 const company1 = new Company();
 
 console.log(user1);
 console.log(company1);
+
+new CustomMap('map');
